81f8e2a7b26da41192f4dca4a3320ac151d0c81f
rathoreisha <118837526+rathoreisha@users.noreply.github.com>
2022-12-17 16:55:01 +0530

    Update Stringnullornot.java

--- Stringnullornot.java
+++ Stringnullornot.java
@@ -2,27 +2,17 @@ package ifelseproject;
 
 public class Stringnullornot {
 	
-	 public static boolean isStringNull(String str)
-	    {
-	  
-	        if (str == null)
-	            return true;
-	        else
-	            return false;
-	    }
+	
 	  
 	    // Driver code
 	    public static void main(String[] args)
 	    {
-	        String str1 = "GeeksforGeeks";
-	        String str2 = null;
-	  
-	        System.out.println("Is string [" + str1
-	                           + "] null? "
-	                           + isStringNull(str1));
-	        System.out.println("Is string [" + str2
-	                           + "] null? "
-	                           + isStringNull(str2));
+	        String str1 = "Isha";
+		    
+		    if(str1==null)
+			    system.out.println("Empty");
+		    else
+			    system.out..println("not empty");
 	    }
 
 }
